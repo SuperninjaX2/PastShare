02e9e294bb0e7f4180ad680ebcfb9c5a28c23906
SuperninjaX2 <80552583+SuperninjaX2@users.noreply.github.com>
2023-07-14 10:44:47 +0200

    Add files via upload

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,10 @@
+let counts = setInterval(updated,1000);
+let upto = 0.0;
 
+function updated() {
+  let count = document.getElementById("counter");
+  count.innerHTML = upto++;
+  if (upto === 14.1) {
+    clearInterval(counts);
+  }
+}
